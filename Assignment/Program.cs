1bc549a7bef3dd0dce5a35b2c03f801c2962d341
AhmedZhran404 <zxcvbnmahmed13579@gmail.com>
2025-07-07 20:57:20 +0300

    Add project files.

---
+++ Assignment/Program.cs
@@ -0,0 +1,375 @@
+﻿using System.Text;
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+
+            /*
+             1- . Write a program that prints an identity matrix using for loop, in other 
+                words takes a value n from the user and shows the identity table of size n * n. 
+             */
+            int[,] arr04 = (int[,])Array.CreateInstance(typeof(int), 2, 4);
+
+            Console.Write("Enter Your Number N Of Matrix: ");
+            bool isParse = int.TryParse(Console.ReadLine(), out int n);
+            if (isParse)
+            {
+
+                int[,] identityMatrix = (int[,])Array.CreateInstance(typeof(int), n, n);
+                for (int i = 0; i < n; i++)
+                {
+                    identityMatrix[i, i] = 1;
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.Write($"{identityMatrix[i, j]}  ");
+                    }
+                    Console.WriteLine();
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("InVaild Number");
+            }
+
+
+            #endregion
+
+
+            #region Q2
+
+            /*
+             2- Write a program in C# Sharp to find the sum of all elements of the array. 
+             */
+
+            int[] nums = { 1, 2, 3, 4, 5, 6 };
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+
+            Console.WriteLine(sum);
+
+            #endregion
+
+
+            #region Q3
+
+            /*
+             * 3- Write a program in C# Sharp to merge two arrays of the same size sorted in 
+                ascending order.
+             */
+
+
+            int[] nums01 = new int[] { 10, 20, 30, 40, 80, 60, 70 }; // 4
+            int[] nums02 = new int[] { 5, 6, 7, 8, 87, 76, 78 }; // 4
+
+            int size = nums01.Length + nums02.Length;
+
+            int[] Num03 = new int[size];
+
+            // Method 0ne1
+
+            for (int i = 0; i < size; i++)
+            {
+                if (i < nums01.Length)
+                {
+                    Num03[i] = nums01[i % nums01.Length]; // [0 % 4] => 0 1 2 3  
+                }
+                else if (i >= nums01.Length)
+                {
+                    Num03[i] = nums02[i % nums01.Length]; // 
+                }
+            }
+
+            foreach (int i in Num03)
+            {
+                Console.Write($"{i} ");
+            }
+
+            // Method 2
+
+
+
+              #endregion
+
+            #region Q4
+
+            /*
+             Write a program in C# Sharp to find maximum and minimum element in an 
+               array  = { 2, 4, 2, 6, 6, 8, 9, 10, 30, 59, 58 }; 
+             */
+
+            int[] numbers01 = { 2, 4, 2, 6, 6, 8, 9, 10, 30, 59, 58 };
+
+            Array.Sort(numbers01);
+
+            int maximum = numbers01[0];
+            int minimum = numbers01[numbers01.Length - 1];
+
+
+            Console.WriteLine($"Minimum Number in This Array is: {minimum} , And Maximum Number is: {maximum}");
+
+
+
+            #endregion
+
+
+            #region Q5
+
+            /*
+             5- Write a program in C# Sharp to find the second largest element in an array. 
+             */
+
+            Console.Write("Enter The Size Of Array: ");
+            bool isParseConverted = int.TryParse(Console.ReadLine(), out int sizeOfArray);
+
+            int[] numsOfArray = new int[sizeOfArray];
+            int maxNumber;
+            int secondMaxNumber = 0;
+            Console.WriteLine("-------- Enter Your Array ---------------");
+            for (int i = 0; i < sizeOfArray; i++)
+            {
+                Console.Write($"Please Enter Element{i + 1}: ");
+                bool isParse01 = int.TryParse(Console.ReadLine(), out numsOfArray[i]);
+            }
+            Console.WriteLine("-----------------------------------------");
+            Array.Sort(numsOfArray);
+            maxNumber= numsOfArray[ numsOfArray.Length - 1 ];
+
+            for(int  i = 0; i< sizeOfArray; i++)
+            {
+                if(maxNumber == numsOfArray[i + 1])
+                {
+                    secondMaxNumber = numsOfArray[i];
+                    break;
+                }
+            }
+
+            Console.WriteLine($"the second largest element in an array {secondMaxNumber}");
+            #endregion
+
+
+            #region Q6
+            /*
+              Consider an Array of Integer values with size N, having values as     
+                in this Example 
+                [ 7 , 0 , 0 , 0 , 0 , 5 , 6 , 7 , 5 , 0 , 7 , 5 , 3 ]
+                write a program find the longest distance between Two equal cells. In this example. The distance 
+                is measured by the number Of cells- for example, the distance between the first and the fourth 
+                cell is 2 (cell 2 and cell 3). 
+                In the example above, the longest distance is between the first 7 and the 
+                10th 7, with a distance of 8 cells, i.e. the number of cells between the 1st 
+                And the 10th 7s. 
+                Note: - Array values will be taken from the user - If you have input like 1111111 then the distance is the number of 
+                Cells between the first and the last cell. 
+             */
+
+
+            Console.Write("Please, Entre The Size of Array: ");
+            bool isConvert = uint.TryParse(Console.ReadLine(), out uint sizearr);
+            if (isConvert)
+            {
+
+                int[] arr01 = new int[sizearr];
+                int Maxdistace = 0;
+                int element = 0;
+
+                for (int i = 0; i < sizearr; i++)
+                {
+                    Console.Write($"Please Enter Element {i + 1}: ");
+                    bool isParse01 = int.TryParse(Console.ReadLine(), out arr01[i]);
+
+                   
+                    if (!isParse)
+                    {
+                        Console.WriteLine("Invalid input. Please enter an integer.");
+                        i--; 
+                    }
+                }
+
+                for (int i = 0; i < sizearr; i++)
+                {
+                    for (int j = i + 1; j < sizearr; j++)
+                    {
+
+                        if (arr01[i] == arr01[j])
+                        {
+                                int distance = j - i - 1;
+
+                                if (distance > Maxdistace)
+                                {
+                                    Maxdistace = distance;
+                                    element = arr01[i];
+                                }
+                        }
+                    }
+                }
+
+                Console.WriteLine($"Max Distance is: {Maxdistace}, For Element {element}");
+            }
+
+            #endregion
+
+
+            #region Q7
+
+
+            /*
+            7- Given a list of space separated words, reverse the order of the words.                                                                                     
+               Input: this is a test  Output: test a is this 
+               Input: all your base  Output: base your all 
+               Input: Word   Output: Word 
+               Note :  
+               Check the Split Function (Member in String Class) Output will be a Single Console.WriteLine 
+               Statement 
+            */
+
+
+            // Method One
+            Console.Write("Please , Enter Your Statment: ");
+            string message = Console.ReadLine() ?? "";
+            bool hasDigit = message.Any(char.IsDigit);
+            if (message != "" && !hasDigit)
+            {
+                StringBuilder result = new StringBuilder("");
+                string word = "";
+                // this is a test
+                for (int i = 0; i < message.Length; i++)
+                {
+                    if (message[i] != ' ')
+                    {
+                        word += message[i];
+
+                    }
+
+                    if (message[i] == ' ' || (i == message.Length - 1))
+                    {
+
+                        result.Insert(0, word + ' '); // a is this 
+                        word = ""; // ""
+                    }
+                }
+
+                Console.WriteLine($"After reverse the order of the words become: {result}");
+            }
+            else
+            {
+                Console.WriteLine("The Message is Empty or Contain Digits");
+            }
+
+            // Method two 
+            Console.Write("Enter a sentence: ");
+            string input = Console.ReadLine() ?? "NotFound";
+
+            string[] words = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Array.Reverse(words);
+
+            string result01 = string.Join(" ", words);
+
+            Console.WriteLine("Reversed: " + result01);
+
+
+            #endregion
+
+
+            #region Q8
+
+
+            /*
+              - Write a program to create two multidimensional arrays of same size. Accept 
+                value from user and store them in first array. Now copy all the elements of 
+                first array on second array and print second array. 
+             */
+
+            Console.Write("Please, Enter Number Of Columns: ");
+            bool isParseColumn = int.TryParse(Console.ReadLine(), out int numColumn);
+            Console.Write("Please, Enter Number Of Rows: ");
+            bool isParseRows = int.TryParse(Console.ReadLine(), out int numRows);
+
+            Console.WriteLine("----------------- Fisrt Array --------------------");
+            if (isParseColumn && isParseRows)
+            {
+                int[,] firstArray = new int[numRows, numColumn];
+                int[,] secondArray = new int[numRows, numColumn];
+
+                for (int i = 0; i < numRows; i++)
+                {
+                    Console.WriteLine($"----- {i + 1} Row ----------");
+                    for (int j = 0; j < numColumn; j++)
+                    {
+                        Console.Write($"Enter Number[{i} , {j}]: ");
+                        bool isParseArr = int.TryParse(Console.ReadLine(), out firstArray[i, j]);
+
+                        if (isParseArr)
+                        {
+
+                            secondArray[i, j] = firstArray[i, j];
+                        }
+                        else
+                        {
+                            --j;
+                        }
+                    }
+
+                }
+
+                Console.WriteLine("----------------- second Array --------------------");
+                for (int i = 0; i < numRows; i++)
+                {
+                    for (int j = 0; j < numColumn; j++)
+                    {
+                        Console.Write($"{secondArray[i, j]} ");
+                    }
+                    Console.WriteLine();
+                }
+
+            }
+
+
+
+
+
+            #endregion
+
+
+            #region Q9
+
+            /*
+             * 9- Write a Program to Print One Dimensional Array in Reverse Order 
+             */
+
+            Console.Write("Pease, Enter Size of Array: ");
+            int.TryParse(Console.ReadLine(), out int sizeArray);
+            int[] D_array = new int[sizeArray];
+            for (int i = 0; i < sizeArray; i++)
+            {
+                Console.Write($"Pease, Enter Element{i + 1}: ");
+                int.TryParse(Console.ReadLine(), out D_array[i]);
+            }
+
+            D_array.Reverse();
+
+            Console.WriteLine("-------- Print Array ---------------");
+
+            foreach (int i in D_array)
+            {
+                Console.Write($"{D_array[i]} ");
+            }
+
+            Console.WriteLine("-----------------------------------");
+
+            #endregion
+
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,226 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ahmed");
+            #region Arrays
+
+            #region 1D Array
+
+            // Array Is Class - Reference Type
+            int[] numbers;
+            // Declare For Reference of Type Array Of Integer
+            // This Reference numbers is refering to The Default Value of The referenceType = null
+            // This  reference can refere to an object of type "Array of Integer"
+            // CLR Will Allocate 4 Bytes For This Reference [Uninitialized] in stack
+            // CLR Will Allocate 0 Bytes in heap
+            numbers = new int[3]; // First method for declare array 
+                                  // CLR Will Allocate 12 Bytes in heap
+                                  // Will Be initialized with The Default Vaule Of int = 0
+                                  // return The Address of The fisrt element in array For "numbers"
+            numbers[0] = 10;
+            numbers[1] = 20;
+            numbers[2] = 30;
+            // numbers[3] = 4; // Will Throw Exception
+
+            // Protective code
+            if (numbers.Length > 3)
+            {
+                Console.WriteLine("Tamam");
+                numbers[3] = 4;
+            }
+
+            Console.WriteLine(numbers[0]); // 10 
+            Console.WriteLine(numbers[1]); // 20
+            Console.WriteLine(numbers[2]); // 30
+
+
+            int[] numbers02 = new int[] { 1, 2, 3, 4 }; // Second Method
+
+            int[] numbers03 = { 1, 2, 3, 4, 5 }; // Third Method
+
+            // int[] numbers04 = [1,2,3,4,5]; // .net 8.0 -> c# 12.0
+
+            Console.WriteLine($"The Length Of Array: {numbers03.Length} And The dimention is: {numbers03.Rank}");
+
+
+
+
+            #endregion
+
+            #region 2D Rectangle
+
+            int[,] Grades = new int[2, 3] { { 10, 20, 30 }, { 40, 50, 60 } };
+
+            // CLR Will Allocate 24Bytes in heap ,
+            // Initialized With data
+
+            for (int r = 0; r < Grades.GetLength(0); r++)
+            {
+                Console.WriteLine($"Student Num#{r + 1}");
+                for (int c = 0; c < Grades.GetLength(1); c++)
+                {
+                    Console.WriteLine($"Grades Of Student: {Grades[r, c]}");
+
+                }
+
+            }
+            uint[,] Grades01 = new uint[2, 3]
+            {
+                     {1,2,3} ,
+                     {4,5,6}
+            };
+
+            for (int r = 0; r < Grades01.GetLength(0); r++)
+            {
+                Console.WriteLine($"Ensert The Value Of Student {r + 1}: ");
+                for (int c = 0; c < Grades01.GetLength(1);)
+                {
+
+                    Console.Write($"Enter Grade Of Subject{c + 1}: ");
+
+                    c = uint.TryParse(Console.ReadLine(), out Grades01[r, c]) ? ++c : c;
+
+                }
+            }
+
+            //   Console.Clear();
+            Console.WriteLine("------------- Print Grades Of Students ----------------");
+
+            for (int r = 0; r < Grades01.GetLength(0); r++)
+            {
+                Console.WriteLine($"Student Num#{r + 1}");
+                for (int c = 0; c < Grades01.GetLength(1); c++)
+                {
+                    Console.WriteLine($"Grades Of Student: {Grades01[r, c]}");
+
+                }
+
+            }
+
+            // ------------------------------------------------
+
+            for (int i = 0; i < Grades01.Length; i++)
+            {
+                Console.WriteLine(Grades01[i / 3, i % 3]); // 
+            }
+            #endregion
+
+
+            #region gaddged Array
+
+            int[][] marks = new int[3][];
+
+            marks[0] = new int[3] { 1, 2, 3 };
+            marks[1] = new int[2] { 4, 5 };
+            marks[2] = new int[1] { 6 };
+
+            Console.WriteLine(marks.Length);
+            for (int i = 0; i < marks.Length; i++)
+            {
+                for (int j = 0; j < marks[i].Length; j++)
+                {
+                    Console.WriteLine(marks[i][j]);
+                }
+            }
+
+
+            #endregion
+
+
+            #region MethodsOfArray
+
+            #region Class Member method
+
+            // Class Member method => A Class Member Method is a method that belongs to the class itself,
+            // not to any specific object of the class.
+            // You can call it without creating an object of the class.
+
+            int[] arr01 = { 5, 4, 10, 2, 7, 6, 12 };
+            int[] arr02 = new int[10];
+
+            Array.Sort(arr01);
+
+            foreach (int i in arr01)
+            {
+                Console.Write($"{i} ");
+            }
+
+            Console.WriteLine("-------------------------------------");
+
+            Array.Copy(arr01, 0, arr02, 0, 4);
+
+
+            foreach (int i in arr02)
+            {
+                Console.Write($"{i} ");
+            }
+
+            Console.WriteLine("-------------------------------------");
+
+            Array.Clear(arr02);
+
+            foreach (int i in arr02)
+            {
+                Console.Write($"{i} ");
+            }
+
+            Console.WriteLine("-------------------------------------");
+
+            Array.ConstrainedCopy(arr01, 5, arr02, 3, 1);
+
+            foreach (int i in arr02)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine("-------------------------------------");
+
+            // int[] arr03  = (int[])  Array.CreateInstance(typeof(int) , 10);
+            // int[,] arr04  = (int[,])  Array.CreateInstance(typeof(int) , 2 , 4);
+
+            // int[,] arr05 = (int[,])Array.CreateInstance(typeof(int), 2, 4 , 3);
+
+
+            Array.IndexOf(arr01, 10); // return index of 10
+
+            Array.LastIndexOf(arr01, 10); //  return Last index of 10
+
+            Console.WriteLine("-------------------------------------");
+
+            #endregion
+
+
+            #region Member Method
+
+            int[] nums01 = { 1, 2, 3, 4 };
+            int[] nums02 = new int[nums01.Length];
+
+
+            nums01.CopyTo(nums02, 0);
+
+            foreach (int i in nums02)
+            {
+                Console.Write($"{i} ");
+            }
+            // --------------------------------------------------
+
+            Console.WriteLine(nums01.GetLength(0)); // 4
+            Console.WriteLine(nums01.GetValue(3)); // 4
+            nums01.SetValue(100, 1); // 4
+            Console.WriteLine(nums01.GetValue(1));
+
+            #endregion
+
+
+            #endregion
+
+            #endregion
+
+
+
+        }
+    }
+}
